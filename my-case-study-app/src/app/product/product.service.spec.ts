8caacb9a083c4d71d87489aa08971a9028039b2a
nansak1 <nansak1ster@gmail.com>
2018-01-23 09:20:53 -0600

    fix service test

--- my-case-study-app/src/app/product/product.service.spec.ts
+++ my-case-study-app/src/app/product/product.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 
 import { ProductService } from './product.service';
 
 describe('ProductService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [ProductService]
     });
   });
